52c894915e035439acc48a64dd83ba87add08ff4
carlinisaac97 <43557586+carlinisaac97@users.noreply.github.com>
2020-04-04 03:51:55 -0400

    Add files via upload

---
+++ practica01.sql
@@ -0,0 +1,12 @@
+﻿-- Database: db_mis_iniciales
+
+-- DROP DATABASE db_mis_iniciales;
+
+CREATE DATABASE db_mis_iniciales
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Paraguay.1252'
+       LC_CTYPE = 'Spanish_Paraguay.1252'
+       CONNECTION LIMIT = -1;
+
