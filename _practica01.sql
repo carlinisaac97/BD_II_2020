8f2d7ce4efe39fd4f02db12eecc91646536c9d71
carlinisaac97 <43557586+carlinisaac97@users.noreply.github.com>
2020-04-04 03:56:08 -0400

    Add files via upload

---
+++ _practica01.sql
@@ -0,0 +1,11 @@
+﻿-- Schema: public
+
+-- DROP SCHEMA public;
+
+CREATE SCHEMA public
+  AUTHORIZATION postgres;
+
+GRANT ALL ON SCHEMA public TO postgres;
+COMMENT ON SCHEMA public
+  IS 'standard public schema';
+
